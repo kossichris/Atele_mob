0b3dd68770831cb1d849ee7f634785ef848ecd2d
chrismal <christian.hounsounou@rintio.com>
2018-11-28 00:06:43 +0100

    hello

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -14,15 +14,15 @@ export class TabsPage {
   tab2Root: any = Tab2Root;
   tab3Root: any = Tab3Root;
 
-  tab1Title = " ";
-  tab2Title = " ";
-  tab3Title = " ";
+  tab1Title = "Profile";
+  tab2Title = "Clients";
+  tab3Title = "Localiser";
 
   constructor(public navCtrl: NavController, public translateService: TranslateService) {
     translateService.get(['TAB1_TITLE', 'TAB2_TITLE', 'TAB3_TITLE']).subscribe(values => {
-      this.tab1Title = values['TAB1_TITLE'];
+     /*  this.tab1Title = values['TAB1_TITLE'];
       this.tab2Title = values['TAB2_TITLE'];
-      this.tab3Title = values['TAB3_TITLE'];
+      this.tab3Title = values['TAB3_TITLE'];*/
     });
   }
 }
